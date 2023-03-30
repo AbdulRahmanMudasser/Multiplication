601be14c318c03112edf1992e5c39a9a0a1b6a9f
Pro <Pro@PROBOOK>
2023-03-30 20:47:11 +0500

    solution done

--- Multiplication/Program.cs
+++ Multiplication/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Multiplication
+{
+    static void Main(string[] args)
+    {
+        // declaring variables
+        int x, y, z;
+
+        // prompting and taking input for x
+        Console.Write("Enter Value Of x:  ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        // prompting and taking input for y
+        Console.Write("Enter Value Of y:  ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        // prompting and taking input for z
+        Console.Write("Enter Value Of z:  ");
+        z = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing 
+
+        // multiplying x, y and z
+        Console.WriteLine("Product Of x, y and z:  " + (x * y * z));
+    }
+}
